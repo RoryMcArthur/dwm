bca802334a8ab90648d8896ea3d2f624c73db6d5
Rory McArthur <rorymcarthur@outlook.com>
2022-03-11 20:12:04 +0100

    added stuff

--- config.h
+++ config.h
@@ -1,9 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
+#define TERM "alacritty"
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "terminus:size=10", "fontawesome" };
 static const char dmenufont[]       = "monospace:size=10";
@@ -19,7 +21,8 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "5", "6", "7", "8" };
+// static const char *tags[] = { "", "", "", "", "5", "6", "7", "8" };
+static const char *tags[] = { "1", "2", "3", "4"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -27,8 +30,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "Gimp",     NULL,       NULL,       1 << 3,       0,           -1 },
 	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ TERM,       NULL,       NULL,       1 << 0,       0,           -1 },
 };
 
 /* layout(s) */
@@ -60,12 +64,14 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *webcmd[]  = { "firefox", NULL };
+static const char *muttcmd[]  = { "alacritty", "-e", "neomutt", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = webcmd } },
+	{ MODKEY|ShiftMask,             XK_m,      spawn,          {.v = muttcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
