827a3d7bdd703446c75aceea0b4e586bfdbb42da
Rory McArthur <rorymcarthur@outlook.com>
2022-03-28 14:05:24 +0200

    Show status bar

--- config.h
+++ config.h
@@ -5,9 +5,9 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "terminus:size=10", "fontawesome" };
+static const char *fonts[]          = { "terminus:size=10", "JoyPixels", "fontawesome" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#535353";
 static const char col_gray2[]       = "#444444";
